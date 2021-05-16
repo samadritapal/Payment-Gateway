17e41c83270ff15e9942cee21f09ab1ea695f05c
SAMADRITA PAL <83641829+samadritapal@users.noreply.github.com>
2021-05-16 17:34:31 +0530

    1st

--- index.js
+++
@@ -1,6 +0,0 @@
-$('#form').submit(function(e) {
-    $('#messages').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
-    $('#messages_content').html('<h4>MESSAGE HERE</h4>');
-    $('#modal').modal('show');
-    e.preventDefault();
-});
